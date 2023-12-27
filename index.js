24e30474036100968821383e7553e32534ae5c59
NabilNagib <nnaby@gmail.com>
2023-12-27 13:35:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.querySelector("main").remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Nabil is the champion";
